920d200b4da348c15e6d89f869a6fc267b41bf8c
Luka <luka.slugecic@gmail.com>
2023-03-09 18:08:43 +0100

    started DTO ModelState validation

---
+++ FuneralHomeWebApi/FuneralHomeWebApi/DTOs/Korisnik.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using DbModels = FuneralHome.DataAccess.SqlServer.Data.DbModels;
+
+namespace FuneralHome.DTOs;
+
+public class Korisnik
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "First name can't be null")]
+    [StringLength(50, ErrorMessage = "First name can't be longer than 50 characters")]
+    public string Ime { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Last name can't be null")]
+    [StringLength(50, ErrorMessage = "Last name can't be longer than 50 characters")]
+    public string Prezime { get; set; } = string.Empty;
+
+    [DataType(DataType.DateTime)]
+    public DateTime DatumRodenja { get; set; }
+    [Required(ErrorMessage = "Address can't be null")]
+    [StringLength(50, ErrorMessage = "Address can't be longer than 50 characters")]
+    public string Adresa { get; set; } = string.Empty;
+    
+    public int Oib { get; set; }
+    [Required(ErrorMessage = "E-mail address can't be null")]
+    [StringLength(50, ErrorMessage = "E-mail address can't be longer than 50 characters")]
+    [Unicode(false)]
+    public string Mail { get; set; } = string.Empty;
+    [Required(ErrorMessage = "Password can't be null")]
+    [StringLength(50, ErrorMessage = "Password can't be longer than 50 characters")]
+    [Unicode(false)]
+    public string Lozinka { get; set; } = string.Empty;
+}
+
+
+public static partial class DtoMapping
+{
+    public static Korisnik ToDto(this DbModels.Korisnik korisnik)
+        => new Korisnik()
+        {
+            Id = korisnik.Id,
+            Ime = korisnik.Ime,
+            Prezime = korisnik.Prezime,
+            DatumRodenja = korisnik.DatumRodenja,
+            Adresa = korisnik.Adresa,
+            Oib = korisnik.Oib,
+            Mail = korisnik.Mail,
+            Lozinka = korisnik.Lozinka,
+        };
+
+    public static DbModels.Korisnik ToDbModel(this Korisnik korisnik)
+        => new DbModels.Korisnik()
+        {
+            Id = korisnik.Id,
+            Ime = korisnik.Ime,
+            Prezime = korisnik.Prezime,
+            DatumRodenja = korisnik.DatumRodenja,
+            Adresa = korisnik.Adresa,
+            Oib = korisnik.Oib,
+            Mail = korisnik.Mail,
+            Lozinka = korisnik.Lozinka
+        };
+}
