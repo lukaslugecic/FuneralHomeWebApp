c3d7adb72cb25a60302668b674b4363ac81282d9
Luka <luka.slugecic@gmail.com>
2023-03-26 17:02:48 +0200

    added icon and minor fixes

--- ClientApp/src/app/app-routing.module.ts
+++ ClientApp/src/app/app-routing.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { HomeModule } from './pages/home/home.module';
+import { RegisterComponent } from './pages/register/register.component';
+import { RegisterModule } from './pages/register/register.module';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent},
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes),
-    HomeModule
+    HomeModule,
+    RegisterModule
   ],
   exports: [RouterModule]
 })
